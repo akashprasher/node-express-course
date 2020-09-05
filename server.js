cd085ef888800f62f6df4be447b69f83966a6959
akashprasher <prince.akash.prasher@gmail.com>
2020-09-05 11:06:55 +0530

    add firt GET route

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 const express = require('express');
 const app = express(); 
 
+const mockUserDAta = [
+    {name:'Mark'},
+    {name:'Jill'}
+]
+
+app.get('/users', function(req, res) {
+    res.json({
+        success: true,
+        message: 'successfully got users. Nice',
+        users: mockUserDAta
+    })
+})
+
 
 app.listen(8000, function() {
     console.log("Server has been started and running");
